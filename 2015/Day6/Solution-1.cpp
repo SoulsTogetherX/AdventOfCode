15a03134b119ac4ec78688551904a2808a8a4fb0
SoulsTogetherX <xalvarez4444@gmail.com>
2025-12-18 11:16:31 -0800

    2015 Day 6

---
+++ 2015/Day6/Solution-1.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <bitset>
+
+using namespace std;
+
+
+const int BOARD_SIZE = 1000;
+
+class Point2D{
+    public:
+        long x, y;
+
+        Point2D() {};
+        Point2D(long x, long y) : x(x), y(y) {};
+
+        string to_string() {
+            return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+        }
+
+        bool operator < (const Point2D& p) const {
+            return x < p.x ||(x == p.x && y < p.y);
+        }
+};
+
+
+const string FILE_NAME = "input.txt";
+ifstream open_file(string filename) {
+    ifstream inputStream;
+
+    cout << endl;
+
+    inputStream.open(filename);
+    if (inputStream.fail()) {
+        cout << "Failed to open file '" << filename << "'." << endl;
+        exit(1);
+    }
+    if (!inputStream.is_open()) {
+        cout << "File '" << filename << "' didn't open." << endl;
+        exit(1);
+    }
+
+    cout << "File '" << filename << "' opened successfully." << endl;
+
+    return inputStream;
+}
+
+void parse_line(string textline, char &op, Point2D &st, Point2D &ed) {
+    stringstream ss(textline);
+    string temp_s;
+    char temp_c;
+
+    ss >> temp_s;
+    if (temp_s == "toggle") {
+        op = '^';
+    } else {
+        ss >> temp_s;
+        if (temp_s == "on") {
+            op = '|';
+        } else {
+            op = '&';
+        }
+    }
+
+    ss >> st.x >> temp_c >> st.y >> temp_s >> ed.x >> temp_c >> ed.y;
+
+    if (st.x > ed.x) {
+        swap(st.x, ed.x);
+    }
+    if (st.y > ed.y) {
+        swap(st.y, ed.y);
+    }
+}
+void process_line(string textline, bitset<BOARD_SIZE> board[BOARD_SIZE]) {
+    char op;
+    Point2D st, ed;
+    parse_line(textline, op, st, ed);
+
+    bitset<BOARD_SIZE> mask;
+    for(int i = st.x; i <= ed.x; i++) {
+        mask.flip(i);
+    }
+
+    if (op == '^') {
+        for(int i = st.y; i <= ed.y; i++) {
+            board[i] ^= mask;
+        }
+    } else if (op == '|') {
+        for(int i = st.y; i <= ed.y; i++) {
+            board[i] |= mask;
+        }
+    } else if (op == '&') {
+        mask.flip();
+        for(int i = st.y; i <= ed.y; i++) {
+            board[i] &= mask;
+        }
+    }
+}
+size_t count_bits(bitset<BOARD_SIZE> board[BOARD_SIZE]) {
+    size_t count = 0;
+
+    for(int i = 0; i < BOARD_SIZE; i++) {
+        count += board[i].count();
+    }
+
+    return count;
+}
+
+
+size_t process_file() {
+    string textline;
+    ifstream inputStream;
+    inputStream = open_file(FILE_NAME);
+
+    bitset<BOARD_SIZE> board[BOARD_SIZE];
+
+    while(getline(inputStream, textline)) {
+        process_line(textline, board);
+    }
+    return count_bits(board);
+}
+
+
+int main() {
+    auto output = process_file();
+    cout << "The number of lit lights is " << output << endl;
+}
