978f16fc2702b8db948682975eda4a8fa674b990
SoulsTogetherX <xalvarez4444@gmail.com>
2026-01-19 10:55:15 -0800

    2017 Day13

---
+++ 2017/Day13/Solution-1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+const string FILE_NAME = "input.txt";
+ifstream open_file(string filename) {
+    ifstream inputStream;
+
+    cout << endl;
+
+    inputStream.open(filename);
+    if (inputStream.fail()) {
+        cout << "Failed to open file '" << filename << "'." << endl;
+        exit(1);
+    }
+    if (!inputStream.is_open()) {
+        cout << "File '" << filename << "' didn't open." << endl;
+        exit(1);
+    }
+
+    cout << "File '" << filename << "' opened successfully." << endl;
+
+    return inputStream;
+}
+
+
+size_t process_file() {
+    string textline;
+    ifstream inputStream;
+    inputStream = open_file(FILE_NAME);
+
+    size_t sum = 0;
+
+    while(getline(inputStream, textline)) {
+    }
+
+    return sum;
+}
+
+
+int main() {
+    auto output = process_file();
+    cout << "The number of times 'XMAS' appears is " << output << endl;
+}
